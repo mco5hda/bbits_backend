ed85ac293e4c7febda6659a733e257d59e296dbd
Juan Carlos Mora <fixed-term.carlos.mora@bosch.com>
2018-12-20 09:20:11 -0600

    Inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+let active = true;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'bbits-backend';
+  ngOnInit(): void {
+    console.log('Inicio');
+  }
+
+  onClickShowMenu(): void {
+    if (active) {
+      const navContent = document.getElementById('nav-content');
+      navContent.classList.remove('M-Navigation-Main--active');
+      const buttonContent = document.getElementById('button-content');
+      buttonContent.classList.remove('M-Navigation-Main__toggle--active');
+      const navigationContent = document.getElementById('navigation-content');
+      navigationContent.classList.remove('is-hidden');
+      const listContenet = document.getElementById('list-content');
+      listContenet.classList.remove('M-Navigation-Main__items--expanded');
+      active = false;
+
+    } else {
+      const navContent = document.getElementById('nav-content');
+      navContent.classList.add('M-Navigation-Main--active');
+      const buttonContent = document.getElementById('button-content');
+      buttonContent.classList.add('M-Navigation-Main__toggle--active');
+      const navigationContent = document.getElementById('navigation-content');
+      navContent.classList.add('is-hidden');
+      const listContenet = document.getElementById('list-content');
+      listContenet.classList.add('M-Navigation-Main__items--expanded');
+      active = true;
+    }
+  }
 }
