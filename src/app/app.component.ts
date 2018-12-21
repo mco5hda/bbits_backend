8ba9a89095d6989515aa08b6c1ea2cd8002bc938
Juan Carlos Mora <fixed-term.carlos.mora@bosch.com>
2018-12-21 14:25:00 -0600

    Menu parcialmente completado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+
 let active = true;
+const MENU_CLASS_ACTIVE = 'M-Navigation-Main--active';
+const videoContent = `<ul id="list-content" class="M-Navigation-Main__items" role="menubar">
+          <li class="M-Navigation__item M-Navigation-Main__item" (mouseover)="onMenuOver('Video')" (mouseleave)="onMenuExit('Video')">
+            <a class="M-Navigation__link M-Navigation-Main__link M-Navigation-Main__linkWithSub M-Navigation__link--active M-Navigation-Main__link--active">
+              <span>IP Cameras</span>
+            </a>
+          </li>
+          <li class="M-Navigation__item M-Navigation-Main__item" (mouseover)="onMenuOver('Access')" (mouseleave)="onMenuExit('Access')">
+            <a class="M-Navigation__link M-Navigation-Main__link M-Navigation-Main__linkWithSub">
+              <span>Analog Cameras</span>
+            </a>
+          </li>
+          <li class="M-Navigation__item M-Navigation-Main__item" (mouseover)="onMenuOver('Intrusion')" (mouseleave)="onMenuExit('Intrusion')">
+            <a class="M-Navigation__link M-Navigation-Main__link M-Navigation-Main__linkWithSub">
+              <span>IP Recording</span>
+            </a>
+          </li>
+          <li class="M-Navigation__item M-Navigation-Main__item" (mouseover)="onMenuOver('Communication')" (mouseleave)="onMenuExit('Communication')">
+            <a class="M-Navigation__link M-Navigation-Main__link M-Navigation-Main__linkWithSub">
+              <span>Analog Recording</span>
+            </a>
+          </li>
+          <li class="M-Navigation__item M-Navigation-Main__item" (mouseover)="onMenuOver('Fire')" (mouseleave)="onMenuExit('Fire')">
+            <a class="M-Navigation__link M-Navigation-Main__link M-Navigation-Main__linkWithSub">
+              <span>Accessories</span>
+            </a>
+          </li>
+        </ul>`;
 
 @Component({
   selector: 'app-root',
@@ -9,9 +38,62 @@ let active = true;
 
 export class AppComponent implements OnInit {
   title = 'bbits-backend';
+
   ngOnInit(): void {
     console.log('Inicio');
   }
+
+  onMenuOver(option): void {
+    switch (option) {
+      case 'Video' :
+        const  navContent = document.getElementById('nav-content');
+        if (navContent.classList[2] === MENU_CLASS_ACTIVE) {
+        } else {
+          document.getElementById('subMenu-video').style.display = 'inherit';
+        }
+        break;
+      case 'Access' :
+        console.log('Access');
+        break;
+      case 'Intrusion' :
+        console.log('Intrusion');
+        break;
+      case 'Communication' :
+        console.log('Communication');
+        break;
+      case 'Fire' :
+        console.log('Fire');
+        break;
+    }
+  }
+
+  onMenuExit(option): void {
+    switch (option) {
+      case 'Video' :
+        const  navContent = document.getElementById('nav-content');
+        if (navContent.classList[2] === MENU_CLASS_ACTIVE) {
+        } else {
+          document.getElementById('subMenu-video').style.display = 'none';
+        }
+        break;
+      case 'Access' :
+        console.log('Access Out');
+        break;
+      case 'Intrusion' :
+        console.log('Intrusion Out');
+        break;
+      case 'Communication' :
+        console.log('Communication Out');
+        break;
+      case 'Fire' :
+        console.log('Fire Out');
+        break;
+    }
+  }
+
+ /*
+  * Hamburger Menu animation
+  * */
 
   onClickShowMenu(): void {
     if (active) {
@@ -21,8 +103,8 @@ export class AppComponent implements OnInit {
       buttonContent.classList.remove('M-Navigation-Main__toggle--active');
       const navigationContent = document.getElementById('navigation-content');
       navigationContent.classList.remove('is-hidden');
-      const listContenet = document.getElementById('list-content');
-      listContenet.classList.remove('M-Navigation-Main__items--expanded');
+      const listContent = document.getElementById('list-content');
+      listContent.classList.remove('M-Navigation-Main__items--expanded');
       active = false;
 
     } else {
@@ -30,10 +112,9 @@ export class AppComponent implements OnInit {
       navContent.classList.add('M-Navigation-Main--active');
       const buttonContent = document.getElementById('button-content');
       buttonContent.classList.add('M-Navigation-Main__toggle--active');
-      const navigationContent = document.getElementById('navigation-content');
       navContent.classList.add('is-hidden');
-      const listContenet = document.getElementById('list-content');
-      listContenet.classList.add('M-Navigation-Main__items--expanded');
+      const listContent = document.getElementById('list-content');
+      listContent.classList.add('M-Navigation-Main__items--expanded');
       active = true;
     }
   }
